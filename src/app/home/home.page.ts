28543aec6d255a553239685b60516feec9682571
scrpgil <scraplbuild@gmail.com>
2019-12-04 02:46:46 +0900

    wip

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Address, AccountHttp, NetworkType} from 'nem2-sdk';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  constructor() {}
-
+    accountInfo: string = "";
+    constructor() {
+      this.getAccountInfo();
+    }
+    getAccountInfo() {
+        const accountHttp = new AccountHttp('http://13.114.200.132:3000');
+        const address = Address.createFromRawAddress(
+            'SCA7ZS-2B7DEE-BGU3TH-SILYHC-RUR32Y-YE55ZB-LYA2',
+        );
+        accountHttp.getAccountInfo(address).subscribe(x => {
+            this.accountInfo = JSON.stringify(x, null, '\t');
+        });
+    }
 }
